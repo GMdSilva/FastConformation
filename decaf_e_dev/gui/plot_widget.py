fd150ca51f09699f96dcf95993e714503c34c5a7
Flavia Maria Galeazzi <fmgaleazzi@flavias-mbp-2.devices.brown.edu>
2024-07-07 13:20:29 -0400

    fix

--- decaf_e_dev/gui/plot_widget.py
+++ decaf_e_dev/gui/plot_widget.py
@@ -14,10 +14,12 @@ class PlotWidget(pg.GraphicsLayoutWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.plots = []
+        self.setBackground('w')
 
     def add_plot(self, x_data, y_data, title, x_label, y_label, color=None, label=None):
         plot_item = self.addPlot(title=title)
         plot_item.addLegend()
+        self.setBackground('w')
         plot_item.plot(x_data, y_data, pen=pg.mkPen(color=color, width=2))
         plot_item.setLabel('left', y_label)
         plot_item.setLabel('bottom', x_label)
