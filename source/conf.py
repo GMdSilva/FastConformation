803a8c1863c4f8d1ca64ac7248ff4c1e7c6f3f98
Flavia Maria Galeazzi <fmgaleazzi@flavias-mbp-2.mynetworksettings.com>
2024-08-18 16:21:58 -0400

    docs

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'fast_ensemble'
+copyright = '2024, Flavia Maria Galeazzi, Pablo Arantes, Gabriel Monteiro Da Silva, Isabel Varghese, Ananya Shukla, Brenda M. Rubenstein'
+author = 'Flavia Maria Galeazzi, Pablo Arantes, Gabriel Monteiro Da Silva, Isabel Varghese, Ananya Shukla, Brenda M. Rubenstein'
+release = '2024'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,17 @@
+.. fast_ensemble documentation master file, created by
+   sphinx-quickstart on Sun Aug 18 16:19:06 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+fast_ensemble documentation
+===========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
